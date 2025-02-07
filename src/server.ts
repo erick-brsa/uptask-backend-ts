4479165c1d989939887b7786ef4e5dab5038bc03
Erick Briones <erickalan518@gmail.com>
2025-02-06 23:53:57 -0600

    Configuración de cors

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 import projectRoutes from './routes/projectRoutes';
 import { connectDB } from './config/db';
+import { corsConfig } from './config/cors';
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
-
+app.use(cors(corsConfig));
 // Habilitar la lectura de JSON
 app.use(express.json());
 
